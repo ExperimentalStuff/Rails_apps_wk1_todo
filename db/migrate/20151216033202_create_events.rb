0261d77adf0d8639d699252dfd1f1021dccf2cdb
Wei-Cheng Yang <rainyday309@gmail.com>
2015-12-16 11:36:03 +0800

    create event model

---
+++ db/migrate/20151216033202_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.text :title
+      t.text :content
+      t.boolean :status
+      t.integer :order
+
+      t.timestamps null: false
+    end
+  end
+end

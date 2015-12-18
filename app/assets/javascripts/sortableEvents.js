fd55b520732d9ff2058f47345839c2da2d4e7fa8
Wei-Cheng Yang <rainyday309@gmail.com>
2015-12-18 13:04:21 +0800

    利用jquery取得每一個event的位置和id並回傳一個陣列包含上述資訊

--- app/assets/javascripts/sortableEvents.js
+++ app/assets/javascripts/sortableEvents.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
 	$("#sortable").sortable();
 	$("#sortable").disableSelection();
+
+	$("#getOrder").on("click", function(){
+		var idpos = [];
+		$(".event").each(function(){
+			var item = $(this).data();
+			item["pos"]= $(".event").index($(this));
+			idpos.push(item);
+		});
+
+		console.log(idpos);
+	});
 });
